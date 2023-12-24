c67c9c7b860fcfbd976555f669428e12cd2091ff
yunji <dbswl330@gmail.com>
2023-12-24 21:22:04 +0900

    Feat: 마이페이지 메뉴 컴포넌트 구현

---
+++ src/components/feature/myPage/MyMenus.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import MyMenu from './MyMenu'
+
+export default function MyMenus() {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="border-4 border-grayscale-50"></div>
+      <MyMenu menuName={'적립금'} detail={'100 원'} />
+      <MyMenu menuName={'쿠폰'} />
+      <div className="border-4 border-grayscale-50"></div>
+      <MyMenu menuName={'주문내역'} />
+      <MyMenu menuName={'선물내역'} />
+      <MyMenu menuName={'자주사는상품'} />
+      <MyMenu menuName={'찜한상품'} />
+      <MyMenu menuName={'상품후기'} />
+      <div className="border-4 border-grayscale-50"></div>
+      <MyMenu menuName={'개인정보수정'} />
+      <div className="border-4 border-grayscale-50"></div>
+
+      <MyMenu menuName={'1:1 문의'} />
+      <MyMenu menuName={'자주하는 질문'} />
+    </div>
+  )
+}
