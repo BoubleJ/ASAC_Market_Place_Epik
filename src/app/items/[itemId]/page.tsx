1bcf182d58a485039900cd0d2f5b6b0462fc70c0
HyunSJ <dudtod1596@gmail.com>
2024-01-10 16:57:11 +0900

    fix add dynamic force to item detail

--- src/app/items/[itemId]/page.tsx
+++ src/app/items/[itemId]/page.tsx
@@ -5,6 +5,8 @@ import ItemDetailInfo from '@/components/feature/item/ItemDetailInfo'
 import ItemMainInfo from '@/components/feature/item/ItemMainInfo'
 import ItemReview from '@/components/feature/item/ItemReview'
 
+export const dynamic = 'force-dynamic'
+
 export default async function ItemPage({ params }: { params: { itemId: number } }) {
   const itemData = await fetchItemDetails(params.itemId)
   const reviewData = await fetchReviews(params.itemId)
