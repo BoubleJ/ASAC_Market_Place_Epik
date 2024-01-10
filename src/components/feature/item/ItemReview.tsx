ac324d7efb8368126d4e771de60a1a2db46005f6
yunji <dbswl330@gmail.com>
2024-01-10 16:18:11 +0900

    Fix: itemDtail 클라이언트 컴포넌트에서 aync/await 사용시 무한 패치 버그 수정
    클라이언트 컴포넌트의 자식 컴포넌트로 서버 컴포넌트를 넣어놓은 코드 수정

--- src/components/feature/item/ItemReview.tsx
+++ src/components/feature/item/ItemReview.tsx
@@ -1,23 +1,29 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
-import { fetchReviews } from '@/api/resource/items'
 import PhotoReviews from '@/components/feature/item/review/PhotoReviews'
 import ReviewList from '@/components/feature/item/review/ReviewList'
 import ReviewNotice from '@/components/feature/item/review/ReviewNotice'
 
-export default async function ItemDetailInfo({ itemId }: { itemId: number }) {
-  const reviewData = await fetchReviews(itemId)
+export default function ItemDetailInfo({ itemId, reviewData }: { itemId: number; reviewData: any }) {
   const review = reviewData.data
+  const tab = useSearchParams().get('tab')
 
   return (
-    <div className="flex flex-col gap-3 p-4">
-      <ReviewNotice />
-      {/* 추후에 추가 (공지)) */}
-      <div className="h-28">
-        <PhotoReviews reviewImages={review.imageUrls} />
-      </div>
-      <div className="border-b-8 my-2 border-b-gray-100"></div>
-      <ReviewList itemId={review.itemId} />
-    </div>
+    <>
+      {tab === 'review' && (
+        <div className="flex flex-col gap-3 p-4">
+          <ReviewNotice />
+          {/* 추후에 추가 (공지)) */}
+          <div className="h-28">
+            <PhotoReviews reviewImages={review.imageUrls} />
+          </div>
+          <div className="border-b-8 my-2 border-b-gray-100"></div>
+          <ReviewList itemId={review.itemId} />
+        </div>
+      )}
+    </>
   )
 }
