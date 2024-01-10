1e3bdf7f7760a93414e9eaa169b5fc300d7fbf5a
yunji <dbswl330@gmail.com>
2024-01-10 10:33:33 +0900

    Fix: new arrivals 페이지에서 runtime = 'edge' 코드 제거

--- src/app/(main)/(home)/new-arrivals/page.tsx
+++ src/app/(main)/(home)/new-arrivals/page.tsx
@@ -1,6 +1,6 @@
 import NewArrivalsList from '@/components/feature/newArrivals/NewArrivalsList'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 
 export default async function NewArrivalsPage() {
   return <NewArrivalsList />
