69d35948bb6c4e50a970dbf83ecd0f1250f47bb6
BoubleJ <dogmnil2007@gmail.com>
2024-01-28 00:52:28 +0900

    FAQItemList apply

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -1,27 +1,13 @@
 import React from 'react'
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import FAQItemList from '@/components/feature/faq/FAQItemList'
 
 export default function page() {
   return (
-    <div className=" text-title-lg">
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger className=" text-body-base">
-            <span className="py-1 px-2  bg-brand-primary-500 rounded-3xl text-body-base text-white">Q</span>교환 반품
-            진행시 배송비가 부과되나요?
-          </AccordionTrigger>
-          <AccordionContent>그럴리가요</AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>교환 반품 진행시 배송비가 부과되나요?</AccordionTrigger>
-          <AccordionContent>그럴리가요</AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>교환 반품 진행시 배송비가 부과되나요?</AccordionTrigger>
-          <AccordionContent>그럴리가요</AccordionContent>
-        </AccordionItem>
-      </Accordion>
+    <div className="w-full h-full p-4">
+      <div className=" text-title-lg">
+        <FAQItemList />
+      </div>
     </div>
   )
 }
---
+++ src/components/feature/faq/FAQItemList.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+
+interface FAQList {
+  Question: string
+  Answer: string
+}
+
+export default function FAQItemList() {
+  const FAQList: FAQList[] = [
+    { Question: '교환(반품) 진행 시, 배송비가 부과되나요?', Answer: '네' },
+    { Question: '선물하기 주문을 취소하고 싶어요', Answer: '아니오' },
+    { Question: '포장재를 회수하기도 하나요?', Answer: '네' },
+    { Question: '아이디, 비밀번호를 잊어버렸습니다', Answer: '네' },
+    { Question: '적립금은 언제 지급되나요?', Answer: '아니오' },
+    { Question: '컬리앱이 켜지지 않아요', Answer: '아니오' },
+    { Question: '이벤트 문자는 오는데, 배송완료 문자만 오지 않습니다', Answer: '아니오' },
+    { Question: '상품 불량인 경우, 교환(반품) 기준이 궁금합니다', Answer: '아니오' },
+  ]
+
+  return (
+    <>
+      {FAQList.map((item, idx) => {
+        return (
+          <Accordion type="single" collapsible className="w-full" key={idx}>
+            <AccordionItem value="item-1">
+              <AccordionTrigger className=" text-body-base">
+                <div>
+                  <span className="py-1 px-2  bg-brand-primary-500 rounded-3xl text-body-base text-white">Q</span>
+                  <span className="pl-3">{item.Question}</span>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="text-body-base">
+                <div className="pl-10">{item.Answer}</div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        )
+      })}
+    </>
+  )
+}
