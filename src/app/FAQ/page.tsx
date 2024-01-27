8e86f5ea03fdd1dc2679430663821f0690470b32
BoubleJ <dogmnil2007@gmail.com>
2024-01-27 18:02:17 +0900

    accordion padding delete

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
-
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 
 export default function page() {
   return (
-    <div className=' text-title-lg' >page</div>
+    <div className=" text-title-lg">
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger className=" text-body-base">
+            <span className="py-1 px-2  bg-brand-primary-500 rounded-3xl text-body-base text-white">Q</span>교환 반품
+            진행시 배송비가 부과되나요?
+          </AccordionTrigger>
+          <AccordionContent>그럴리가요</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>교환 반품 진행시 배송비가 부과되나요?</AccordionTrigger>
+          <AccordionContent>그럴리가요</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>교환 반품 진행시 배송비가 부과되나요?</AccordionTrigger>
+          <AccordionContent>그럴리가요</AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
   )
 }
