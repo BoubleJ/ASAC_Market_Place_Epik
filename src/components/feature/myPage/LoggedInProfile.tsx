fb7d5f491fbe415f0e98606f151664724e16ddeb
BoubleJ <dogmnil2007@naver.com>
2024-03-01 22:09:11 +0900

    feat: 마이페이지 멤버등급별 색상구현

--- src/components/feature/myPage/LoggedInProfile.tsx
+++ src/components/feature/myPage/LoggedInProfile.tsx
@@ -4,15 +4,16 @@ import { memberType } from '@/app/(main)/(myPage)/myPage/page'
 import { Button } from '@/components/ui/button'
 
 export default function LoggedInProfile({ member }: { member: memberType }) {
+
   return (
     <div>
-      <div className="flex gap-4 items-center ">
-        <div className="border-2 border-brand-primary-400 rounded-lg p-4 text-brand-primary-500 text-title-md">
+      <div className="flex items-center gap-4 ">
+        <div className={`border-tier-color-${member.memberGrade} rounded-lg border-2 p-4 text-title-md text-brand-primary-500`}>
           일반
         </div>
         <span className="text-title-md">{member?.memberName} 님</span>
       </div>
-      <div className="flex mt-4 justify-around gap-2 text-button-sm text-center">
+      <div className="mt-4 flex justify-around gap-2 text-center text-button-sm">
         <Button variant={'gray'} size={'lg'}>
           전체등급 보기
         </Button>
